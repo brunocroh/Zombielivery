df1e755138449f40a0d607c3cc4519e633bfd61c
Luisfp0 <luisfernandodepaulosousa0020@gmail.com>
2023-04-29 17:26:26 -0300

    Teste

--- src/Game/Game.gd
+++ src/Game/Game.gd
@@ -5,9 +5,9 @@ var myNode = preload("../Player/player.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-  var myNode_instance =  myNode .instance() 
-  get_tree().get_root().add_child(myNode_instance)
-  myNode_instance.global_transform = global_transform
+	var myNode_instance =  myNode .instance() 
+	get_tree().get_root().add_child(myNode_instance)
+	myNode_instance.global_transform = global_transform
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
--- src/Player/Player.gd
+++ src/Player/Player.gd
@@ -5,28 +5,59 @@ var screen_size # Size of the game window.
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-  screen_size = get_viewport_rect().size
+	screen_size = get_viewport_rect().size
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-  var velocity = Vector2.ZERO # The player's movement vector.
-  if Input.is_action_pressed("move_right"):
-      velocity.x += 1
-  if Input.is_action_pressed("move_left"):
-      velocity.x -= 1
-  if Input.is_action_pressed("move_down"):
-      velocity.y += 1
-  if Input.is_action_pressed("move_up"):
-      velocity.y -= 1
+	var velocity = Vector2.ZERO # The player's movement vector.
+	if Input.is_action_pressed("move_right"):
+		velocity.x += 1
+	if Input.is_action_pressed("move_left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("move_down"):
+		velocity.y += 1
+	if Input.is_action_pressed("move_up"):
+		velocity.y -= 1
 
-  if velocity.length() > 0:
-    velocity = velocity.normalized() * speed
-    $AnimatedSprite2D.play()
-  else:
-    $AnimatedSprite2D.stop()
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+		$AnimatedSprite2D.play()
+	else:
+		$AnimatedSprite2D.stop()
 
-  position += delta * velocity
+	position += delta * velocity
 
-  position.x = clamp(position.x, 0, screen_size.x)
-  position.y = clamp(position.y, 0, screen_size.y)
+	position.x = clamp(position.x, 0, screen_size.x)
+	position.y = clamp(position.y, 0, screen_size.y)
+
+## HealthBar
+func update_health():
+	var healthbar = $healthbar
+	var health = healthbar.value
+	if health >= 100:
+		healthbar.visible = false
+	else:
+		healthbar.visible = true
+
+func _on_regintimer_timeout():
+	pass # Replace with function body.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
