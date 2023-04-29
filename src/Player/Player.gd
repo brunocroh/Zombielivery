e93223711043b116d4e83e97da5514ed75c40ae0
Luisfp0 <luisfernandodepaulosousa0020@gmail.com>
2023-04-29 18:02:47 -0300

    Resolvido conflito

--- src/Player/Player.gd
+++ src/Player/Player.gd
@@ -1,14 +1,28 @@
 extends Area2D
 
-@export var speed = 400 # How fast the player will move (pixels/sec).
-var screen_size # Size of the game window.
+@export var speed = 400 
+var screen_size 
+var health = 100
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	screen_size = get_viewport_rect().size
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	player_movement(delta)
+	update_health()
+
+
+
+
+
+
+
+
+
+
+
+func player_movement(delta):
+	
 	var velocity = Vector2.ZERO # The player's movement vector.
 	if Input.is_action_pressed("move_right"):
 		velocity.x += 1
@@ -25,6 +39,11 @@ func _process(delta):
 	else:
 		$AnimatedSprite2D.stop()
 
+	if velocity.x < 0:
+		$AnimatedSprite2D.flip_h = true
+	else:
+		$AnimatedSprite2D.flip_h = false
+
 	position += delta * velocity
 
 	position.x = clamp(position.x, 0, screen_size.x)
@@ -33,14 +52,7 @@ func _process(delta):
 ## HealthBar
 func update_health():
 	var healthbar = $healthbar
-	var health = healthbar.value
-	if health >= 100:
-		healthbar.visible = false
-	else:
-		healthbar.visible = true
-
-func _on_regintimer_timeout():
-	pass # Replace with function body.
+	print($healthbar)
 
 
 
@@ -59,15 +71,5 @@ func _on_regintimer_timeout():
 
 
 
-<<<<<<< HEAD
-=======
-
-  if velocity.x < 0:
-    $AnimatedSprite2D.flip_h = true
-  else:
-    $AnimatedSprite2D.flip_h = false
-
-  position += delta * velocity
->>>>>>> 32c580a088a9b6be539351ac7b62456ee91e4882
 
 
