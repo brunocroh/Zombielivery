f758d4db83f84bfb2598b404499bf2c5e684e731
Bruno Pinheiro <brunocroh@gmail.com>
2023-04-29 22:54:36 -0300

    map

--- src/Map/Map.gd
+++ src/Map/Map.gd
@@ -1,13 +1,8 @@
 extends Area2D
 
-@onready var map = preload("res://src/Map/Map.tscn")
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
-  var mapa = map.instantiate()
-  mapa.position = Vector2(0, 0)
-  get_tree().current_scene.add_child(mapa)
-  
+  pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
