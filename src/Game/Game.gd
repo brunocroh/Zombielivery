abb0f18ff820a6a796c1d58e96e70fd68b508ede
Bruno Pinheiro <brunocroh@gmail.com>
2023-04-30 17:28:52 -0300

    merge

--- src/Game/Game.gd
+++ src/Game/Game.gd
@@ -11,61 +11,61 @@ var oldRenderedTiles = []
 var dicTiles = {}
 
 func on_enemy_health_depleted(_enemy):
-  print('enemy')
-  get_tree().current_scene.call_deferred('remove_child', _enemy)
-  _enemy.call_deferred('queue_free')
+	print('enemy')
+	get_tree().current_scene.call_deferred('remove_child', _enemy)
+	_enemy.call_deferred('queue_free')
 
 
 func _on_area_exited(_area: Area2D, mapa):
-  var x = mapa.position.x
-  var y = mapa.position.y
+	var x = mapa.position.x
+	var y = mapa.position.y
 
-  oldRenderedTiles = renderedTiles
-  renderedTiles = []
+	oldRenderedTiles = renderedTiles
+	renderedTiles = []
 
-  create_tiles(x, y)
+	create_tiles(x, y)
 
-  for n in oldRenderedTiles:
-    if n != null:
-      get_tree().current_scene.call_deferred('remove_child', n)
-      n.queue_free()
+	for n in oldRenderedTiles:
+		if n != null:
+			get_tree().current_scene.call_deferred('remove_child', n)
+			n.queue_free()
 
 func create_tile(x, y):
-  var mapa = map.instantiate()
-  mapa.position = Vector2(x, y)
-  mapa.area_exited.connect(func(area): _on_area_exited(area, mapa))
-  get_tree().current_scene.call_deferred('add_child', mapa)
-  return mapa
+	var mapa = map.instantiate()
+	mapa.position = Vector2(x, y)
+	mapa.area_exited.connect(func(area): _on_area_exited(area, mapa))
+	get_tree().current_scene.call_deferred('add_child', mapa)
+	return mapa
 
 func create_tiles(x, y):
-  for i in range(-1, 4):
-    for j in range(-1, 4):
-      var _x = x + (i - 1) * tileSize
-      var _y = y + (j - 1) * tileSize
-      var dicKey = str(_x, ',', _y)
-      if (not dicTiles.has(dicKey)):
-        dicTiles[dicKey] = 'rendered'
+	for i in range(-1, 4):
+		for j in range(-1, 4):
+			var _x = x + (i - 1) * tileSize
+			var _y = y + (j - 1) * tileSize
+			var dicKey = str(_x, ',', _y)
+			if (not dicTiles.has(dicKey)):
+				dicTiles[dicKey] = 'rendered'
 
-        renderedTiles.append(
-          create_tile(x + (i - 1) * tileSize, y + (j - 1) * tileSize)    
-        )
+				renderedTiles.append(
+					create_tile(x + (i - 1) * tileSize, y + (j - 1) * tileSize)    
+				)
 
-        dicTiles = {}
+				dicTiles = {}
 
 func spawn_enemy(x, y):
-  var _enemy = enemy.instantiate()
-  _enemy.position = Vector2(x, y)
-  _enemy.health_depleted.connect(func(): on_enemy_health_depleted(_enemy))
-  get_tree().current_scene.call_deferred('add_child', _enemy)
-  return _enemy
+	var _enemy = enemy.instantiate()
+	_enemy.position = Vector2(x, y)
+	_enemy.health_depleted.connect(func(): on_enemy_health_depleted(_enemy))
+	get_tree().current_scene.call_deferred('add_child', _enemy)
+	return _enemy
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-  create_tiles(0, 0)
-  renderedEnemies.append(
-    spawn_enemy(10, 10)
+	create_tiles(0, 0)
+	renderedEnemies.append(
+		spawn_enemy(10, 10)
 )
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
-  pass
+	pass
