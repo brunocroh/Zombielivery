dbac2ae3ac0d111763c03fdbedbd84ec403ee648
Bruno Pinheiro <brunocroh@gmail.com>
2023-05-01 15:32:29 -0300

    git status damage enemies

--- src/Enemy/Enemy.gd
+++ src/Enemy/Enemy.gd
@@ -1,34 +1,45 @@
 extends CharacterBody2D
 
 var target 
-var SPEED = 500
+@export var SPEED = 500
 
-var health = GlobalScript.healthBasicEnemy
+var health = 100
 signal health_depleted
 signal enemy_attack
 
 func _ready():
+	update_health()
 	add_to_group(GlobalScript.grupoInimigo)
 
-
 func _physics_process(delta):
+	update_health()
 	if target:
 		var _velocity = global_position.direction_to(target.position)
 		move_and_collide(_velocity * SPEED * delta)
-
 
 func _on_area_2d_body_entered(body):
 	print(body.name)
 	if body.name == "Player":
 		target = body
 
-
 func _on_area_2d_body_exited(body):
 	print(body.name)
 	if body.name == "Player":
 		target = null
 
-
 func _on_attack_range_body_entered(body):
 	if body.name == "Player":
 		enemy_attack.emit(2)
+
+func update_health():
+	var healthbar = $EnemyHealth
+	healthbar.value = health
+
+	if health <= 0:
+		health_depleted.emit()
+		self.queue_free()
+
+	if health >= 100:
+		healthbar.visible = false
+	else:
+		healthbar.visible = true
--- src/bullet/bullet.gd
+++ src/bullet/bullet.gd
@@ -8,5 +8,9 @@ func _ready():
 	add_to_group(GlobalScript.grupoPlayer)
 
 func _physics_process(delta):
-	
 	var collision_info = move_and_collide(veloc.normalized() * delta * speed)
+
+
+func _on_area_2d_body_entered(body):
+	body.health -= 40
+	self.queue_free()
